4f32554e040ccaac4176fe1d93b5f24daed552b3
zaxxon <zaxxon@57134797-d65f-492c-a18a-0c6163c5b442>
2010-05-13 20:33:41 +0000

    Important change to btb06 to avoid changing anchors position among different runs of btb06

--- trunk/project/scripts/message.m
+++ trunk/project/scripts/message.m
@@ -160,3 +160,7 @@ switch(mcode)
 end
 display('                                                                -')
 display('-----------------------------------------------------------------')
+
+if (numero_sons>0) & (numero_father>0)
+	display('WARNING: You have both father.txt and sons.txt on the root folder of this track. Remove one of them');
+end

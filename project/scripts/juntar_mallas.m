e200e766515c4ac89a79cdaf9c7f8dc936bfe948
zaxxon <zaxxon@57134797-d65f-492c-a18a-0c6163c5b442>
2010-07-09 18:20:03 +0000

    Minor changes

--- project/scripts/juntar_mallas.m
+++ project/scripts/juntar_mallas.m
@@ -228,8 +228,9 @@ message(11);
 %--------------------------------------------------------------------------
     
     function Ntri1=triangulos_remapeados(mapeo1,tri1);
-        
-         rango=1:length(tri1);
+         dim1=size(tri1);
+		 dim1=dim1(1);
+         rango=1:dim1;
          Ntri1=[mapeo1(tri1(rango,1))' mapeo1(tri1(rango,2))' mapeo1(tri1(rango,3))'];
          %for h=1:length(tri1)
          %   Ntri1(h,:)=[mapeo1(tri1(h,1)) mapeo1(tri1(h,2)) mapeo1(tri1(h,3))];

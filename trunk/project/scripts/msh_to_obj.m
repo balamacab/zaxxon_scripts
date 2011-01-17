d6362a46bfd15ac05adbf90a9481f54948bc01d6
zaxxon <zaxxon@57134797-d65f-492c-a18a-0c6163c5b442>
2011-01-17 04:09:56 +0000

    msh_to_obj works also with quads

--- trunk/project/scripts/msh_to_obj.m
+++ trunk/project/scripts/msh_to_obj.m
@@ -3,27 +3,44 @@ function msh_to_obj(fichero_nodos,fichero_elements)
 [numero x y z]=textread(fichero_nodos,'%d %f %f %f');
 
 fid=fopen(fichero_elements,'r');
-contents=fread(fid,inf);
+fid_w=fopen(strcat('salida\test','.obj'),'w');
+contador=1;
+while (feof(fid)!=1)
+    todo=fscanf(fid,'%d',3)
+	if length(todo)>0
+		tipo=todo(2);
+		if (tipo==2) || (tipo==3)
+			numtags=todo(3);
+			lostags=fscanf(fid,'%d',numtags)
+			id_superficie(contador)=lostags(1);
+			id_particular(contador)=lostags(2);
+			if tipo==2 %Triang
+				datos=fscanf(fid,'%d %d %d\n',3)
+				n1(contador)=datos(1);
+				n2(contador)=datos(2);
+				n3(contador)=datos(3);
+				n4(contador)=-1;
+			elseif tipo==3
+				datos=fscanf(fid,'%d %d %d %d\n',4)
+				n1(contador)=datos(1);
+				n2(contador)=datos(2);
+				n3(contador)=datos(3);
+				n4(contador)=datos(4);
+			end
+			contador=contador+1;
+		else
+		fgets(fid);
+		end
+	end
+end
 fclose(fid);
-cadena=char(contents)';
-todo=sscanf(cadena,'%d',inf);
-numtags=todo(3);
-tam_registro=numtags+6;
-id_superficie=todo(4:tam_registro:end);
-n1=todo((numtags+4):tam_registro:end);
-n2=todo((numtags+5):tam_registro:end);
-n3=todo((numtags+6):tam_registro:end);
-id_particular=todo(5:tam_registro:end);
-
 [nada,orden]=sort(id_superficie);
 n1=n1(orden);
 n2=n2(orden);
 n3=n3(orden);
+n4=n4(orden);
 id_particular=id_particular(orden);
 
-		fid_w=fopen(strcat('salida\test','.obj'),'w');
-		
-		
 		fprintf(fid_w,'v %f %f %f\r\n',[x y -z]')
 		
 		for g=1:length(x)
@@ -33,14 +50,18 @@ id_particular=id_particular(orden);
 		v1=n1;
 		v2=n2;
 		v3=n3;
+		v4=n4;
 		id_anterior=-1;
 		for h=1:length(n1)
 				if id_superficie(h)~=id_anterior
 					fprintf(fid_w,'g group%02d\r\n',id_anterior);
 					id_anterior=id_superficie(h);
-				else
 				end
-			    fprintf(fid_w,'f %d/%d/%d %d/%d/%d %d/%d/%d\r\n',v1(h),v1(h),v1(h),v2(h),v2(h),v2(h),v3(h),v3(h),v3(h));
+				if v4(h)==-1
+					fprintf(fid_w,'f %d/%d/%d %d/%d/%d %d/%d/%d\r\n',v1(h),v1(h),v1(h),v2(h),v2(h),v2(h),v3(h),v3(h),v3(h));
+				else
+					fprintf(fid_w,'f %d/%d/%d %d/%d/%d %d/%d/%d %d/%d/%d\r\n',v1(h),v1(h),v1(h),v2(h),v2(h),v2(h),v3(h),v3(h),v3(h),v4(h),v4(h),v4(h));
+				end
 		end			
 
 		fclose(fid_w);

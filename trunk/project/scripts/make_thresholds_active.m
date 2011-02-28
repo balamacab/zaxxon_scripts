fbc6fc20e0013d1cde157c1e5f831c273fad0f39
zaxxon <zaxxon@57134797-d65f-492c-a18a-0c6163c5b442>
2011-02-28 22:04:46 +0000

    limits.geo now has the limits

--- trunk/project/scripts/make_thresholds_active.m
+++ trunk/project/scripts/make_thresholds_active.m
@@ -6,5 +6,13 @@ fid2=fopen('threshold.txt','r');
 contenido=fread(fid2,inf);
 fwrite(fid,contenido);
 fclose(fid2);
+
+%Si encontramos limites, los añadimos tras los puntos que definen los thresholds
+fid_limits=fopen('salida\limits.geo','r');
+if fid_limits~=-1
+	contents=fread(fid_limits,Inf); 
+	fclose(fid_limits);
+	fwrite(fid,contents);
+end
 fclose(fid); 
 end

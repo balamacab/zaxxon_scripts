16fc8be0b8f887800004d579fe5e4423c6c53498
zaxxon <zaxxon@57134797-d65f-492c-a18a-0c6163c5b442>
2011-03-19 16:46:30 +0000

    New poner_muro

--- project/scripts/poner_muro.m
+++ project/scripts/poner_muro.m
@@ -9,12 +9,12 @@ function  poner_muro(LODOut)
 % 
 % El autor no acepta ninguna responsabilidad por cualquier daño resultante del uso de este código.
 
-  fichero_de_nodos='nodos_conaltura.txt';
+fichero_de_nodos='nodos_conaltura.txt';
 fichero_elements='elements.txt';
 id_conducible=111;
 id_noconducible=222;
 if (nargin>1)
-    display('Ejemplo: poner_muro() o poner_muro(300)');
+    display('Example: poner_muro() o poner_muro(300)');
     display(' being 300 the LOD out of the walls');
     display(' ');
     display('Salida: listado de nodos que deben definir el muro');
@@ -22,7 +22,7 @@ if (nargin>1)
     return;
 end
 if nargin==0
-   LODOut=5; %POr defecto 5m de LODOut
+   LODOut=1; %POr defecto 5m de LODOut
 end
 
 display('Leyendo ficheros');
@@ -38,8 +38,10 @@ display('Calculando');
 malla1=zeros(1,length(numero));
 malla2=zeros(1,length(numero));
 
-%Marcamos qué nodos forman parte de la parte interna y externa del muro
+edges_list=[];
+contador=1;
 for h=1:length(n1)
+	%Marcamos qué nodos forman parte de la parte interna y externa del muro
     if id_conducible==id_superficie(h)
         malla1(n1(h))=1;
         malla1(n2(h))=1;
@@ -50,9 +52,21 @@ for h=1:length(n1)
         malla2(n2(h))=1;
         malla2(n3(h))=1;
     end
+	
+	%Creamos un listado de "edges" o líneas que forman las faces de la parte no conducible
+	if id_noconducible==id_superficie(h)
+		%Debo anotar 3 edges. El número menor en primer lugar
+		edges_list(contador,1)=min(n1(h),n2(h));
+		edges_list(contador,2)=max(n1(h),n2(h));
+		edges_list(contador+1,1)=min(n2(h),n3(h));
+		edges_list(contador+1,2)=max(n2(h),n3(h));
+		edges_list(contador+2,1)=min(n3(h),n1(h));
+		edges_list(contador+2,2)=max(n3(h),n1(h));
+		contador=contador+3;
+	end
 end
 
-	interseccion=((malla1+malla2)==2);
+interseccion=((malla1+malla2)==2);
 nodos_interseccion=numero(find(interseccion==1)); %Listado desordenado de los nodos que forman parte de ambas mallas
 
 %A efectos de comprobación generamos un par de ficheros para gmsh
@@ -81,11 +95,74 @@ fclose(fid);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+%Solo interesan los edges que sean contorno, que son los que solo pertenecen a un triángulo y que por tanto no están duplicados
+
+[edges_list(:,1) indices]=sort(edges_list(:,1));
+edges_list(:,2)=edges_list(indices,2);
+diferentes=unique(edges_list(:,1));
+for h=1:length(diferentes)
+    [indices]=find(diferentes(h)==edges_list(:,1));
+    edges_list(indices,2)=sort(edges_list(indices,2));
+end
+
+accepted=ones(length(edges_list),1);
+for h=1:length(edges_list)-1
+	if accepted(h)==1
+		if (edges_list(h+1,1)==edges_list(h,1)) && (edges_list(h+1,2)==edges_list(h,2)) %Si son el mismo edge es que no es un edge-contorno
+			accepted(h)=0; 
+			accepted(h+1)=0; 
+		end
+	end
+end
+size(edges_list);
+accepted;
+
+contour_edges=[];
+contador=1;
+for h=1:length(accepted)
+	if accepted(h)==1
+		contour_edges(contador,:)=edges_list(h,:);
+		contador=contador+1;
+	end
+end
+
+
 display('Acabando')
 
-%Empezamos por uno y los ordenamos según su proximidad
-%ordenados=ordenar(nodos_interseccion,x,y,z,val_pred)
-  [ordenados saltos]=ordenar_b(nodos_interseccion,n1,n2,n3,id_superficie,id_conducible,id_noconducible,x,z);
+%Empezamos por un nodo que pertenezca a ambas superfices, la 111 y la 222
+inicial=nodos_interseccion(1);
+
+ordenados(1)=inicial;
+contador=1;
+nuevo=-1;
+size(contour_edges);
+edge_usado=zeros(length(contour_edges),1);
+while (nuevo~=inicial) 
+	%Buscamos el edge al que pertenece el punto
+	 pos1=find(ordenados(contador)==contour_edges(:,1));
+	 pos2=find(ordenados(contador)==contour_edges(:,2));
+	 if length(pos1)>0
+		for g=1:length(pos1)
+			if edge_usado(pos1(g))==0
+				pos=pos1(g);
+				columna=2;
+			end
+		end
+     end
+     if length(pos2)>0
+	 	for g=1:length(pos2)
+			if edge_usado(pos2(g))==0
+				pos=pos2(g);
+				columna=1;
+			end
+        end
+     end
+     
+	 contador=contador+1;
+	 nuevo=contour_edges(pos,columna); %Anotamos ese punto como siguiente en el contorno
+	 edge_usado(pos)=1;
+	 ordenados(contador)=nuevo;
+end
 
 fid=fopen('salida\ordenados.geo','w');
 for h=1:length(ordenados)
@@ -111,48 +188,21 @@ inicio2=strcat(...
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%fid=fopen('final_muro1.txt','r');
-%final1=fread(fid,inf);
-%fclose(fid)
+
 
 tam_wall=25; %Cada tam_wall nodos, un muro nuevo
-pos_saltos=find(saltos==1);
-if length(pos_saltos)==0
-    pos_saltos=[1 length(saltos)]; %Un único tramo
-else
-    pos_saltos(1)=1;
-	pos_saltos(end)=1;
-end
-
-%La ruta más larga va en un sentido, las cortas en otro (de la larga no se puede salir si estás dentro, a las cortas no se debe poder entrar si estás fuera)
-longitud_muro=0;
-for h=1:length(pos_saltos)/2;	
-    inicio=pos_saltos(h*2-1);
-	final=pos_saltos(h*2);
-	if (final-inicio)>longitud_muro
-		muro_largo=h;
-		longitud_muro=final-inicio;
-	end
-end	
-
 
 fid=fopen('final_muro2.txt','r');final2=fread(fid,inf);fclose(fid);
 
 fid=fopen('salida\muros.txt','w');
 contador=0;
-for h=1:length(pos_saltos)/2;	
-    inicio=pos_saltos(h*2-1);
-	final=pos_saltos(h*2);
-	%listado=[ordenados(inicio:final) ordenados(inicio)]; %Si queremos cerrar las superficies
-	listado=ordenados(inicio:final);
-	sentido_curva(x(listado)',z(listado)');
-	h==muro_largo;
-	if ((sentido_curva(x(listado)',z(listado)')==1) & (h==muro_largo)) || (((sentido_curva(x(listado)',z(listado)')==0) & (h~=muro_largo)))%Si es de sentido horario y es la larga, la reconvertimos
+listado=ordenados;
+sentido_curva(x(listado)',z(listado)');
+if ((sentido_curva(x(listado)',z(listado)')==1))%Si es de sentido horario y es la larga, la reconvertimos
       listado=fliplr(flipud(listado));
-    end
-    anyadidos=imprimir(fid,listado,x,y,z,inicio2,final2,tam_wall,contador,LODOut);
-	contador=contador+anyadidos;
 end
+anyadidos=imprimir(fid,listado,x,y,z,inicio2,final2,tam_wall,contador,LODOut);
+contador=contador+anyadidos;
 fclose(fid);
 
 fid=fopen('salida\muros.txt','r');
@@ -164,33 +214,6 @@ fid=fopen('salida\muros.txt','w');
 cad_inicial=sprintf('  <Walls count="%d">',contador);
 cad_final='  </Walls>';
 fprintf(fid,'%s\r\n %s\r\n %s\r\n',cad_inicial,char_contenido,cad_final);
-fclose(fid);
-
-%-----------------------Muro invertido
-
-fid=fopen('final_muro2_invertido.txt','r');final2=fread(fid,inf);fclose(fid);
-
-fid=fopen('salida\muros_invertidos.txt','w');
-
-contador=0;
-for h=1:length(pos_saltos)/2;	
-    inicio=pos_saltos(h*2-1);
-	final=pos_saltos(h*2);
-	listado=[ordenados(inicio:final) ordenados(inicio)];
-    anyadidos=imprimir(fid,listado,x,y,z,inicio2,final2,tam_wall,contador,LODOut);
-	contador=contador+anyadidos;
-end
-fclose(fid);
-
-fid=fopen('salida\muros_invertidos.txt','r');
-contenido=fread(fid,inf);
-fclose(fid);
-
-fid=fopen('salida\muros_invertidos.txt','w');
-char_contenido=char(contenido)';
-cad_inicial=sprintf('  <Walls count="%d">\r\n',contador);
-cad_final='  </Walls>\r\n';
-fprintf(fid,'%s',strcat(cad_inicial,char_contenido,cad_final));
 fclose(fid);
 
 message(13);
@@ -237,72 +260,3 @@ fwrite(fid,final);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-function [aceptado saltos]=ordenar_b(nodos_interseccion,n1,n2,n3,id_superficie,id_conducible,id_noconducible,x,z)
-saltos=zeros(1,length(nodos_interseccion));
-aceptado=zeros(1,length(nodos_interseccion));
-usado=zeros(1,length(nodos_interseccion));
-aceptado(1)=nodos_interseccion(1);
-usado(1)=1; %Máscara de usados en nodos_interseccion
-actual=1; %Contador
-
-indice_conducibles=find(id_superficie==id_conducible);
-elementos_conducibles=[n1(indice_conducibles) n2(indice_conducibles) n3(indice_conducibles)];
-indice_noconducibles=find(id_superficie==id_noconducible);
-elementos_noconducibles=[n1(indice_noconducibles) n2(indice_noconducibles) n3(indice_noconducibles)];
-
-while sum(aceptado>0)<length(aceptado)
-	resto_nodos=nodos_interseccion(find(usado==0));
-	elements_implicados1=(elementos_conducibles(:,1)==aceptado(actual));
-	elements_implicados2=(elementos_conducibles(:,2)==aceptado(actual));
-	elements_implicados3=(elementos_conducibles(:,3)==aceptado(actual));
-	%Lista de triángulos que tienen al nodo actual como uno de sus nodos
-	c_implicados=find(elements_implicados1.+elements_implicados2.+elements_implicados3);
-
-	elements_implicados1=(elementos_noconducibles(:,1)==aceptado(actual));
-	elements_implicados2=(elementos_noconducibles(:,2)==aceptado(actual));
-	elements_implicados3=(elementos_noconducibles(:,3)==aceptado(actual));
-	%Lista de triángulos que tienen al nodo actual como uno de sus nodos
-	nc_implicados=find(elements_implicados1.+elements_implicados2.+elements_implicados3);
-
-%El nodo que buscamos tiene que pertenecer al mismo tiempo al resto de nodos, a los triángulos conducibles implicados y a los no conducibles
-	  
-	%Lista de nodos implicados por la parte conducible
-	  lista=[];
-          for h=1:length(c_implicados)
-	    lista=[lista elementos_conducibles(c_implicados(h),1)];
-	    lista=[lista elementos_conducibles(c_implicados(h),2)];
-	    lista=[lista elementos_conducibles(c_implicados(h),3)];
-          end
-          lista_c=lista;
-
-	  lista=[];
-          for h=1:length(nc_implicados)
-	    lista=[lista elementos_noconducibles(nc_implicados(h),1)];
-	    lista=[lista elementos_noconducibles(nc_implicados(h),2)];
-	    lista=[lista elementos_noconducibles(nc_implicados(h),3)];
-          end
-          lista_nc=lista;
-
-          [C IC INC]=intersect(lista_c,lista_nc);
-          
-          [C IA pos]=intersect(lista_c(IC),resto_nodos);  
-
-	actual=actual+1;
-	
-	if length(pos)==0  %No hemos encontrado un nodo común a los dos triángulos que pertenezca también a la frontera
-		display('Jump');		
-		pos=1; %We jump to another segment
-		salto(actual-1)=1; %Marcamos tanto el último nodo del anterior, como el primero del actual
-		salto(actual)=1;
-	end
-
-	aceptado(actual)=resto_nodos(pos(1));%Para el primer punto hay dos opciones
-	usado(find(nodos_interseccion==aceptado(actual)))=1;
-	if mod(actual,1000)==0
-		mensaje=sprintf('%.2f\r\n',actual/length(nodos_interseccion));
-		display(mensaje);
-	end
-end
-
-aceptado=aceptado(find(aceptado>0));
-%aceptado(end+1)=nodos_interseccion(1); %Unir principio y final

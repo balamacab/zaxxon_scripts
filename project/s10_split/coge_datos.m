be586948da479bfc6a8a82e74c93287561b87117
zaxxon <zaxxon@57134797-d65f-492c-a18a-0c6163c5b442>
2011-03-28 15:18:21 +0000

    Small fixes

--- project/s10_split/coge_datos.m
+++ project/s10_split/coge_datos.m
@@ -1,9 +1,9 @@
 [numero_padres caminos]=look_for_father_or_sons('..\father.txt',1);
 if numero_padres==0 
-   system('copy ..\s4_terrain\salida\elements.txt .')
-   system('copy ..\s6_hairpins\salida\nodos_conaltura.txt .')
+   system('copy ..\s4_terrain\salida\elements.txt . 2>NUL')
+   system('copy ..\s6_hairpins\salida\nodos_conaltura.txt . 2>NUL')
 end
-system('copy ..\Venue\nodos.mat .');
-system('copy ..\Venue\porcentajes.mat .');
+system('copy ..\Venue\nodos.mat . 2>NUL');
+system('copy ..\Venue\porcentajes.mat . 2>NUL');
 
 message(14);

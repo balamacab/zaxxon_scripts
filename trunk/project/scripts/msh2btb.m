baf034518ab508d7ed4e5c1302d6b3f4b25b1cad
zaxxon <zaxxon@57134797-d65f-492c-a18a-0c6163c5b442>
2011-08-02 20:33:54 +0000

    Two scripts for importing terrain to BTB added

---
+++ trunk/project/scripts/msh2btb.m
@@ -0,0 +1,27 @@
+function msh2btb(dimx,dimy)
+trocea_malla
+system('copy salida\elements.txt elements.txt');
+system('copy salida\nodos.txt nodos_conaltura.txt');
+system('copy nodos_conaltura.txt ..\s4_terrain\salida\.');
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+fid=my_fopen('..\s4_terrain\salida\listado_anchors.txt','w');
+
+[numero x y z]=textread('nodos_conaltura.txt','%d %f %f %f');
+temp=y;
+y=z;
+z=temp;
+
+tamanyo=length(x);
+fprintf(fid,'     <TerrainAnchors count="%d">\n',tamanyo);
+for h=1:tamanyo
+    fprintf(fid,'       <TerrainAnchor>\n         <Position x="%f" y="%f" z="%f" />\n       </TerrainAnchor>\n',x(h),y(h),z(h));
+end
+fprintf(fid,'     </TerrainAnchors>\n');
+
+my_fclose(fid);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+procesar_elementstxt_mt(dimx,dimy,0,-1);%Sin mezclar con imágenes de fondo y sin unir a carretera

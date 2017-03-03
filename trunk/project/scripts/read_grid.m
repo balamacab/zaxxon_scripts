0372ebdd5d76f23378d036e8ff3f80259fa5a3e7
zaxxon <zaxxon@57134797-d65f-492c-a18a-0c6163c5b442>
2017-03-03 22:02:07 +0000

    bug en mallado

--- trunk/project/scripts/read_grid.m
+++ trunk/project/scripts/read_grid.m
@@ -2,17 +2,16 @@ function read_grid(vergrafica)
 %---
 % Descargado de http://foro.simracing.es/bobs-track-builder/3815-tutorial-ma-zaxxon.html
 %---
-% Este código NO es software libre. Su uso implica la aceptación de las condiciones del autor,
-% condiciones que explícitamente prohiben tanto la redistribución como su uso con fines comerciales.
-% Asimismo queda prohibida la realización de cualquier modificación que no sea para estricto uso personal 
+% Este cï¿½digo NO es software libre. Su uso implica la aceptaciï¿½n de las condiciones del autor,
+% condiciones que explï¿½citamente prohiben tanto la redistribuciï¿½n como su uso con fines comerciales.
+% Asimismo queda prohibida la realizaciï¿½n de cualquier modificaciï¿½n que no sea para estricto uso personal 
 % y sin finalidad comercial.
 % 
-% El autor no acepta ninguna responsabilidad por cualquier daño resultante del uso de este código.
+% El autor no acepta ninguna responsabilidad por cualquier daï¿½o resultante del uso de este cï¿½digo.
 
 if nargin>0
-    display('Uso: sin parámetros. Compruebe que la información en pantalla coincide con los parámetros usados con make_grid')
+    display('Uso: sin parï¿½metros. Compruebe que la informaciï¿½n en pantalla coincide con los parï¿½metros usados con make_grid')
 end
-
 
 S=load('dimensiones.mat','num_filas','num_columnas','guarda_calentamiento');
 num_filas=S.num_filas;
@@ -24,6 +23,7 @@ pos1=[];
 pos2=[];
 altura=[];
 [errores,nadena]=system('dir salida\grid*relleno.kml /b');
+%[errores,nadena]=system('ls -1 salida/grid*relleno.kml')
 numero_ficheros=length(findstr(nadena,'.')); %Hay tantos ficheros como puntos en nadena
 
 pos1=zeros(num_filas*num_columnas,1);
@@ -32,7 +32,7 @@ altura=zeros(num_filas*num_columnas,1);
 
 contador=1;
 for g=1:numero_ficheros
-    nombre=sprintf('salida\\grid%.3d_relleno.kml',g);
+    nombre=sprintf('salida/grid%.3d_relleno.kml',g);
     [nada1 nada2 trozoy]=leer_datos(nombre);
     % De cada num_filas+guarda_calentamiento datos, los guarda_calentamiento primeros son para tirar
       for h=1:length(nada1)/(num_filas+guarda_calentamiento)

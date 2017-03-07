2e1aa9db3c411b8df4b33232d0ade5faebc4b99c
zaxxon <zaxxon@57134797-d65f-492c-a18a-0c6163c5b442>
2017-03-07 22:53:12 +0000

    creando muros en el mallado

--- project/scripts/mallado.m
+++ project/scripts/mallado.m
@@ -306,19 +306,35 @@ rango=(1:length(murox));
 
 offs=length(rango);
 fid=fopen('salida/nodosmuroizdo.txt','w');
-fprintf(fid,'%d %f %f %f\n',[rango' x' y' z']');
-fprintf(fid,'%d %f %f %f\n',[(rango+offs)' x' y' (z+4)']'); %4 metros de alto
+fprintf(fid,'%d %f %f %f\n',[rango' murox' muroy' muroz']');
+fprintf(fid,'%d %f %f %f\n',[(rango+offs)' murox' muroy' (muroz+4)']'); %4 metros de alto
 fclose(fid);
 
+dmuro=sqrt(sum(diff(murox').^2+diff(muroy').^2,2));
+dmuro_acumulada=cumsum([0; dmuro])/10;
 triangulos=[]
 for h=1:length(murox)-1;
     triangulos= [triangulos; h h+1 h+offs];
     triangulos= [triangulos; h+offs h+1 h+offs+1];
+    murou(h)=dmuro_acumulada(h);
+    murou(h+offs)=dmuro_acumulada(h);
+    murov(h)=0;
+    murov(h+offs)=1;
 end
+murou(offs)=dmuro_acumulada(offs);
+murov(offs)=0;
+murov(offs+offs)=1;
 
 fid=fopen('salida/elementsmuroizdo.txt','w');
 fprintf(fid,'%d 2 2 55 0 %d %d %d  \n',[(1:longitud(triangulos,3))' triangulos ]');%zona 55, por poner algo
 fclose(fid);
+
+
+
+fid=fopen('salida/texturas.txt','w');
+fprintf(fid,'vt %f %f\n',[murou;murov]);
+fclose(fid);
+
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
